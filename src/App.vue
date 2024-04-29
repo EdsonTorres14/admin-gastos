e0b50439cf3d71bed40d6132e3553927ecb6b79c
EdsonTorres14 <edsontorresrios@gmail.com>
2024-04-29 00:00:51 -0600

    105 - Primeros pasos con el Formulario de Presupuesto

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <script setup>
-
+import Presupuesto from './components/Presupuesto.vue';
 </script>
 
 <template>
   <div>
     <header>
       <h1>Planificador de Gatos</h1>
+      <div class="contededor-header contenedor sombra">
+        <Presupuesto />
+      </div>
     </header>
 
   </div>
@@ -23,9 +26,10 @@
 }
 
 html {
-  font-size:62.5%;
+  font-size: 62.5%;
   box-sizing: border-box;
 }
+
 *,
 *:before,
 *:after {
@@ -38,7 +42,7 @@ body {
   background-color: var(--gris-claro);
 }
 
-h1{
+h1 {
   font-size: 4rem;
 }
 
@@ -46,14 +50,33 @@ h2 {
   font-size: 3rem;
 }
 
-header{
+header {
   background-color: var(--azul);
 }
 
 header h1 {
   padding: 3rem 0;
   margin: 0;
-  color:var(--blanco);
+  color: var(--blanco);
   text-align: center;
+}
+
+.contenedor {
+  width: 90%;
+  max-width: 80rem;
+  margin: 0 auto;
+}
+
+.contenedor-header {
+  margin-top: -5rem;
+  transform: translate(5rem);
+  padding: 5rem;
+}
+
+.sombra {
+  box-shadow: 0px 10px 15px -3px rgba(0, 0, 0, 0.1);
+  background-color: var(--blanco);
+  border-radius: 1.2rem;
+  padding: 5rem;
 }
 </style>
