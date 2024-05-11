10e9e5ad307f4fb3f4ea0ed63b52ee24784dcb6a
Edson Torres <66502687+EdsonTorres14@users.noreply.github.com>
2024-05-10 18:30:33 -0600

    113. Formatear cantidades como Dinero

--- src/components/ControlPresupuesto.vue
+++ src/components/ControlPresupuesto.vue
@@ -1,5 +1,6 @@
 <script setup>
 import imagen from '../assets/img/grafico.jpg'
+import { formatearCantidad } from '../helpers'
 
 const props = defineProps({
     presupuesto: {
@@ -22,12 +23,12 @@ const props = defineProps({
 
             <p>
                 <span>Presupuesto:</span>
-                $ {{ presupuesto }}
+                {{ formatearCantidad(presupuesto) }}
             </p>
 
             <p>
                 <span>Disponible:</span>
-                $ {{ disponible }}
+                {{ formatearCantidad(disponible) }}
             </p>
             <p>
                 <span>Gastado:</span>
