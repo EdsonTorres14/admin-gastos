d61e479f5e7816a4632a8462bcef2541ef56aa45
Edson Torres <66502687+EdsonTorres14@users.noreply.github.com>
2024-05-24 18:29:28 -0600

    127. Primeros pasos mostrando los Gastos

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,9 @@ import { ref, reactive } from 'vue'
 import Presupuesto from './components/Presupuesto.vue';
 import ControlPresupuesto from './components/ControlPresupuesto.vue';
 import Modal from './components/Modal.vue';
+import Gasto from './components/Gasto.vue';
 import iconoNuevoGasto from './assets/img/nuevo-gasto.svg';
 import { generarId } from './helpers';
-
 const modal = reactive({
   mostrar: false,
   animar: false
@@ -70,6 +70,10 @@ const guardarGasto = () => {
       </div>
     </header>
     <main v-if="presupuesto > 0">
+      <div class="listado-gastos contenedor">
+        <h2>{{ gastos.length > 0 ? 'Gastos' : 'No hay gastos' }}</h2>
+        <Gasto v-for="gasto in gastos" :key="gasto.id" :gasto="gasto" />
+      </div>
       <div class="crear-gasto">
         <img :src="iconoNuevoGasto" alt="Icono nuevo gasto" @click="mostrarModal" />
       </div>
@@ -156,5 +160,14 @@ header h1 {
 .crear-gasto img {
   width: 5rem;
   cursor: pointer;
+}
+
+.listado-gastos {
+  margin-top: 10rem;
+}
+
+.listado-gastos h2 {
+  font-weight: 900;
+  color: var(--gris-oscuro);
 }
 </style>
